3be9422e2352644a4dd976f14ae0936e483975b6
gaurangibhangale <gaurangibhangale@gmail.com>
2015-02-26 15:53:59 -0800

    Adding cancer info and taxonomy

--- CancerBigDataProject/app.js
+++ CancerBigDataProject/app.js
@@ -3,7 +3,8 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , meher = require('./routes/meher')
-   , gaurangi = require('./routes/gaurangi')
+  , gaurangi = require('./routes/gaurangi')
+  , hanifa = require('./routes/hanifa')
   , http = require('http')
   , path = require('path')
   , crypto = require('crypto')
@@ -49,60 +50,25 @@ app.post('/signup',meher.saveUser);
 app.post('/signin',meher.signindo);
 app.get('/signout',meher.signout);
 
-//home page and track symptoms-->Gaurangi
+//Gaurangi javascript
+//home page and track symptoms
 app.get('/trackSymptoms',gaurangi.tracksymptoms);
 app.get('/ViewHistory', gaurangi.viewhistory);
 app.post('/addSymptoms',gaurangi.addSymptoms);
-
+//contactus and about us
+app.get('/contact-us',gaurangi.contactus);
+app.get('/about-us',gaurangi.aboutus);
+//cancer information
+app.get('/bladder-cancer',gaurangi.bladder);
+app.get('/breast-cancer',gaurangi.breast);
+app.get('/colorectal-cancer',gaurangi.colorectal);
+app.get('/lung-cancer',gaurangi.lung);
+app.get('/prostate-cancer',gaurangi.prostate);
+app.get('/vulvar-cancer',gaurangi.vulvar);
+//cancer taxanomy
+app.get('/taxanomy',hanifa.taxanomy);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-
-/**
- * Module dependencies.
- */
-/*
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , gaurangi = require('./routes/gaurangi')
-
-  , http = require('http')
-  , path = require('path');
-
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-// home page and track symptoms-->Gaurangi
-app.get('/trackSymptoms',gaurangi.tracksymptoms);
-app.get('/ViewHistory', gaurangi.viewhistory);
-app.post('/addSymptoms',gaurangi.addSymptoms);
-
-
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-*/
--- CancerBigDataProject/routes/gaurangi.js
+++ CancerBigDataProject/routes/gaurangi.js
@@ -1,62 +1,163 @@
+exports.tracksymptoms = function(req, res) {
+	if (req.session.fname == undefined) {
+		res.redirect("/");
+	} else {
+		var sessionset;
+		if (req.session.fname == undefined) {
+			var sessionset = "";
+		} else {
+			var sessionset = "set";
+		}
 
-exports.tracksymptoms = function(req, res){
-  res.render('tracksymptoms', { title: 'TrackSymtoms' });
+		res.render('tracksymptoms', {
+			title : 'TrackSymtoms',
+			session1 : sessionset
+		});
+	}
 };
 
-//exports.ViewHistory = function(req, res){
-//	  res.render('viewhistory', { title: 'ViewHistory' });
-//	};
+exports.contactus = function(req, res) {
+	res.render('contact-us', {
+		title : 'Contact Us'
+	});
+};
 
+exports.aboutus = function(req, res) {
+	res.render('about-us', {
+		title : 'About Us'
+	})
+}
 
+exports.bladder = function(req, res) {
+	var sessionset;
+	if (req.session.fname == undefined) {
+		var sessionset = "";
+	} else {
+		var sessionset = "set";
+	}
+	res.render('bladder-cancer', {
+		title : 'bladder-cancer',
+		session1 : sessionset
+	})
+}
+exports.breast = function(req, res) {
+	var sessionset;
+	if (req.session.fname == undefined) {
+		var sessionset = "";
+	} else {
+		var sessionset = "set";
+	}
+	res.render('breast-cancer', {
+		title : 'breast-cancer',
+		session1 : sessionset
+	})
+}
+exports.colorectal = function(req, res) {
+	var sessionset;
+	if (req.session.fname == undefined) {
+		var sessionset = "";
+	} else {
+		var sessionset = "set";
+	}
+	res.render('colorectal-cancer', {
+		title : 'colorectal-cancer',
+		session1 : sessionset
+	})
+}
+exports.lung = function(req, res) {
+	var sessionset;
+	if (req.session.fname == undefined) {
+		var sessionset = "";
+	} else {
+		var sessionset = "set";
+	}
+	res.render('lung-cancer', {
+		title : 'lung-cancer',
+		session1 : sessionset
+	})
+}
+exports.prostate = function(req, res) {
+	var sessionset;
+	if (req.session.fname == undefined) {
+		var sessionset = "";
+	} else {
+		var sessionset = "set";
+	}
+	res.render('prostate-cancer', {
+		title : 'prostate-cancer',
+		session1 : sessionset
+	})
+}
+exports.vulvar = function(req, res) {
+	var sessionset;
+	if (req.session.fname == undefined) {
+		var sessionset = "";
+	} else {
+		var sessionset = "set";
+	}
+	res.render('vulvar-cancer', {
+		title : 'vulvar-cancer',
+		session1 : sessionset
+	})
+}
 
 /**
  * New node file
  */
 mysqldb = require('./Connection.js');
 
-exports.addSymptoms = function(req, res){
-//	if(req.session.fname == undefined){
-//		res.redirect("/");
-//	}
-//	else {
-//		
+exports.addSymptoms = function(req, res) {
+	if (req.session.fname == undefined) {
+		res.redirect("/");
+	} else {
+
 		var input = JSON.parse(JSON.stringify(req.body));
 		var data = {
-				cancer_type : input.cancertype,
-				date : input.date,
-				severity : input.severity,
-				affected_location: input.location,
-				description : input.description
+			cancer_type : input.cancertype,
+			date : input.date,
+			severity : input.severity,
+			affected_location : input.location,
+			description : input.description
 		};
 		var connection = mysqldb.getConnection();
 		console.log(data);
 		connection.connect();
-		
-		var query = connection.query("Insert into track_symptoms set ? ", data, function(err, rows){
-			if(err)
-				{
-				console.log("Error inserting : %s", err);
-				res.redirect('/');
-				}
-			//console.log(rows);
-		});
-		//res.redirect('/ViewHistory');
-		//console.log(rows)
+
+		var query = connection.query("Insert into track_symptoms set ? ", data,
+				function(err, rows) {
+					if (err) {
+						console.log("Error inserting : %s", err);
+						res.redirect('/');
+					}
+					// console.log(rows);
+				});
+		// res.redirect('/ViewHistory');
+		// console.log(rows)
 		connection.end();
 	}
-
-exports.viewhistory = function(req, res){
-	
-		var connection = mysqldb.getConnection();
-
-		connection.connect();
-		connection.query("Select * from track_symptoms", function(err, rows){
-			if(err)
-				console.log("Error fetching results : %s", err);
-			res.render('ViewHistory',{data: rows});
-		});
-		connection.end();
-	//}
-		
 }
 
+exports.viewhistory = function(req, res) {
+	if (req.session.fname == undefined) {
+		res.redirect("/");
+	} else {
+		var connection = mysqldb.getConnection();
+
+		var sessionset;
+		if (req.session.fname == undefined) {
+			var sessionset = "";
+		} else {
+			var sessionset = "set";
+		}
+		connection.connect();
+		connection.query("Select * from track_symptoms", function(err, rows) {
+			if (err)
+				console.log("Error fetching results : %s", err);
+			res.render('ViewHistory', {
+				data : rows,
+				session1 : sessionset
+			});
+		});
+		connection.end();
+	}
+}
